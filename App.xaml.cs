cbb77771c57e2c13703ac2b5e1de0ee45ac1c590
Suttiporn S <YeE2540@outlook.com>
2023-12-06 00:48:22 +0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppMyNote
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
